81e12e06024083cfb3e7ac82c5db6787061bd140
GeunTae KIM <60951051+Rootstar07@users.noreply.github.com>
2020-12-18 23:49:07 +0900

    1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var button = document.getElementById("button");
 var input = document.getElementById("input");
 var list = document.getElementById("list");
+var cnt = 1;
 
 button.addEventListener('click', clickButton);
 
@@ -9,5 +10,13 @@ function clickButton()
     var temp = document.createElement('li');
     temp.innerHTML = input.value;
     list.appendChild(temp);
+
+    temp.setAttribute("id", "li"+cnt);
+    temp.innerHTML += "<button type='button' onclick='remove("+cnt+")'>삭제</button>";
 }
 
+function remove(cnt)
+{
+    var li = document.getElementById("li"+cnt)
+    list.removeChild(li);
+}
